1f77006fc02f087af94c661bcc4fcc86ae944338
Dev Jadiya <devjadiya15@gmail.com>
2025-03-12 21:59:35 +0530

    Removed import

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import * as React from 'react';
 import AttributeGrid from '../components/AttributeGrid';
 import Container from '../components/Container';
 import Hero from '../components/Hero';
-import BlogPreviewGrid from '../components/BlogPreviewGrid';
 import Highlight from '../components/Highlight';
 import Layout from '../components/Layout/Layout';
 import ProductCollectionGrid from '../components/ProductCollectionGrid';
